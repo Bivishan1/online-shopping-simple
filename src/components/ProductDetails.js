95b5091f3bc84069bdccd87cc09a17aca72d2013
unknown <sapkotabivishan@gmail.com>
2024-10-16 21:31:59 +0545

    home page data fetching & rendering complete

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+function ProductDetails() {
+    return (
+        <div>ProductDetails</div>
+    )
+}
+
+export default ProductDetails
